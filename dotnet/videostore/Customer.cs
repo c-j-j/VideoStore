3355129d34ebaabc88223add535eceab3c49395f
= <chrisjordan1987@gmail.com>
2015-03-24 14:33:29 +0000

    pulled ability to determine rental amount outside of for loop

--- dotnet/videostore/Customer.cs
+++ dotnet/videostore/Customer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace videostore
@@ -33,42 +32,43 @@ namespace videostore
             String result = "Rental Record for " + GetName() + "\n";
             foreach (var rental in rentals)
             {
-                double thisAmount = 0;
-
-                // determines the amount for each line
-                switch (rental.GetMovie().GetPriceCode())
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (rental.GetDaysRented() > 2)
-                            thisAmount += (rental.GetDaysRented() - 2) * 1.5;
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += rental.GetDaysRented() * 3;
-                        break;
-                    case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (rental.GetDaysRented() > 3)
-                            thisAmount += (rental.GetDaysRented() - 3) * 1.5;
-                        break;
-                }
-
+                var rentalAmount = DetermineRentalAmount(rental);
                 frequentRenterPoints++;
-
                 if (rental.GetMovie().GetPriceCode() == Movie.NEW_RELEASE
                         && rental.GetDaysRented() > 1)
                     frequentRenterPoints++;
 
-                result += "\t" + rental.GetMovie().GetTitle() + "\t" + string.Format("{0:F1}", thisAmount) + "\n";
-                totalAmount += thisAmount;
-
+                result += "\t" + rental.GetMovie().GetTitle() + "\t" + string.Format("{0:F1}", rentalAmount) + "\n";
+                totalAmount += rentalAmount;
             }
 
             result += "You owed " + string.Format("{0:F1}", totalAmount) + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points\n";
 
-
             return result;
+        }
+
+        static double DetermineRentalAmount(Rental rental)
+        {
+            double thisAmount = 0;
+            // determines the amount for each line
+            switch (rental.GetMovie().GetPriceCode())
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.GetDaysRented() > 2)
+                        thisAmount += (rental.GetDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.GetDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (rental.GetDaysRented() > 3)
+                        thisAmount += (rental.GetDaysRented() - 3) * 1.5;
+                    break;
+            }
+            return thisAmount;
         }
 
         public double GetTotalAmount()
